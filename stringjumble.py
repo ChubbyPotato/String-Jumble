76f3e35b03ea605058bff9faa24a5b35abb76409
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-09-28 14:38:43 -0400

    Updated from Brython Server: 9/28/2015 2:38:43 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
 Author: Suhan Gui
-Credit: Me!
+Credit: http://stackoverflow.com/questions/12794141/reverse-input-in-python
 
 Assignment: String Jumble
 
@@ -25,5 +25,8 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-s=input("Please enter a string of text (the bigger the better): ")
-print("You entered '{0}'. Now jumble it: ".format(s))
+s=input('Please enter a string of text (the bigger the better): ')
+print('You entered "{0}". Now jumble it: '.format(s))
+
+print("{0}".format(' '.join(reversed(s.split(' ')[::-1]))))
+print("{0}".format(' '.join(reversed(s.split(' ')))))
